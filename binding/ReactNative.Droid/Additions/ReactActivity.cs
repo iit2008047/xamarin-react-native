e6db614cbb15c85ef3fc016bd076bfcb3bcf5d34
faizkarim <faiz.karim@sprinklr.com>
2022-11-30 20:41:24 -0500

    [chore] rn68.3

--- binding/ReactNative.Droid/Additions/ReactActivity.cs
+++ binding/ReactNative.Droid/Additions/ReactActivity.cs
@@ -4,14 +4,14 @@ namespace Com.Facebook.React
 {
     public abstract partial class ReactActivity
     {
-        new public int CheckSelfPermission(string permission)
-        {
-            return (int)base.CheckSelfPermission(permission);
-        }
+        //new public int CheckSelfPermission(string permission)
+        //{
+        //    return (int)base.CheckSelfPermission(permission);
+        //}
 
-        new public int CheckPermission(string permission, int pid, int uid)
-        {
-            return (int)base.CheckPermission(permission, pid, uid);
-        }
+        //new public int CheckPermission(string permission, int pid, int uid)
+        //{
+        //    return (int)base.CheckPermission(permission, pid, uid);
+        //}
     }
 }
