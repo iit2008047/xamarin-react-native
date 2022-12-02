7784b0d420a16eaeb6cc0abe3464cdf26bbc567c
faizkarim <faiz.karim@sprinklr.com>
2022-12-01 20:14:42 -0500

    [chore] fixing RN dll issues

--- samples/SampleApp.Droid/MainActivity.cs
+++ samples/SampleApp.Droid/MainActivity.cs
@@ -5,14 +5,14 @@ using Com.Facebook.React.Shell;
 using Com.Facebook.React.Common;
 using Android.Views;
 using Com.Facebook.React.Modules.Core;
-using Android.Support.V7.App;
+using AndroidX.AppCompat.App;
 using Android.Content;
 using Android.Net;
 using Android.Provider;
 
 namespace SampleApp.Droid
 {
-    [Activity(Label = "XamarinReactNative", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    [Activity(Label = "XamarinReactNative", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : AppCompatActivity, IDefaultHardwareBackBtnHandler
     {
         ReactRootView mReactRootView;
